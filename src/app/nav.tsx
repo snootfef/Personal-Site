abb2918559c3362194beeea4b2cd71e5621e3624
snootfef <delingucanada@gmail.com>
2024-02-01 20:44:43 -0500

    Added scroll reactivity and animated some elements

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,8 +1,19 @@
+'use client';
 import Link from "next/link"
+import useScrollPosition from "./useScrollPosition";
+import { useEffect, useState } from "react";
 
 export default function Nav() {
+   const scrollPosition = useScrollPosition();
+   const [scroll, setScroll] = useState(false)
+
+   if (scrollPosition > 400 && !scroll)
+      setScroll(true)
+   else if (scrollPosition < 400 && scroll)
+      setScroll(false)
+
    return (
-      <nav className='flex justify-center items-center min-w-full h-14 fixed bg-white text-black'>
+      <nav className={'flex items-center w-screen h-14 fixed bg-white text-black duration-700 ease-in-out ' + (scroll ? 'pr-0 pl-[60vw]' : 'px-[33vw]')}>
          <Link
             href='/projects'
             className="m-16"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,49 @@
+'use client';
 import Image from "next/image";
 import Nav from './nav'
 import { FaGithub } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Home() {
+
+  function handleClick() {
+    (document.getElementById('bottom') as HTMLInputElement).scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <main className="flex flex-col items-center relative overflow-x-hidden overflow-y-auto no-scrollbar">
       <Nav />
       <section
-        className="flex h-screen w-screen flex-col items-center justify-center pb-24 bg-black text-white"
+        className="flex h-screen w-screen flex-col items-center justify-center bg-black text-white"
       >
         <section
-          className="flex flex-col items-center justify-center"
+          className="flex flex-col items-center justify-center h-full pt-8"
         >
           <p>hi, I'm</p>
           <h1>Delin Gu</h1>
         </section>
-        <section>
+        <section
+          className="pb-8"
+        >
           {/* character icons */}
+          <button
+            onClick={handleClick}
+          >
+            <IoIosArrowDown
+              className="text-4xl opacity-25 hover:opacity-75 ease-in"
+            />
+          </button>
         </section>
       </section>
       <section
         className="flex h-screen w-screen bg-white"
+        id="bottom"
       >
         <section
-          className="w-1/2 m-20"
+          className="w-1/2 m-24 flex flex-col justify-center"
         >
-          <p>
+          <p
+          >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
           <div
@@ -35,7 +53,7 @@ export default function Home() {
           </div>
         </section>
         <div
-          className="w-1/2 m-20"
+          className="w-1/2 m-24"
         >
           asdfasdf
         </div>
