51571d54d9d7d7541cbae43725994687099165a2
snootfef <delingucanada@gmail.com>
2024-04-29 20:55:15 -0400

    Finished up image positioning and styling of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -27,8 +27,9 @@ export default function Page() {
                </h2>
                <section
                   style={{
-                     backgroundImage: "url('/assets/test2.jpg')",
-                     backgroundPosition: "center"
+                     backgroundImage: "url('/assets/about1.jpg')",
+                     backgroundPosition: "left 48% bottom 50%",
+                     backgroundSize: "150%"
                   }}
                   className="rounded-full front flex flex-col items-center justify-center w-[22vw] h-[80vh]"
                >
@@ -52,7 +53,7 @@ export default function Page() {
             className='min-h-screen w-full relative font-playfair'
          >
             <h2
-               className='mt-24 ml-24 italic font-bold text-xl'
+               className='mt-28 ml-24 italic font-bold text-xl text-moss'
             >
                my life currently...
             </h2>
@@ -60,31 +61,71 @@ export default function Page() {
                className='z-40 flex flex-row w-full h-[90vh] relative bg-rice'
             >
                <div
-                  className='my-16 ml-12 mr-4 rounded-full w-[18vw] h-[60vh] bg-moss'
+                  style={{
+                     backgroundImage: "url('/assets/aboutFlowers.jpg')",
+                     backgroundPosition: "center",
+                     backgroundSize: "125%"
+                  }}
+                  className='my-16 ml-20 mr-16 rounded-full w-[18vw] h-[60vh] bg-moss'
                />
                <div
+                  style={{
+                     backgroundImage: "url('/assets/aboutUoft.jpg')",
+                     backgroundPosition: "left 10% top 80%",
+                     backgroundSize: "120%"
+                  }}
                   className='mt-[16rem] rounded-full w-[15vw] h-[45vh] bg-green-400'
                />
                <div
-                  className='mt-56 ml-[22rem] rounded-full w-[20vw] h-[72vh] bg-green-400'
+                  style={{
+                     backgroundImage: "url('/assets/aboutCake.jpg')",
+                     backgroundPosition: "center",
+                     backgroundSize: "140%"
+                  }}
+                  className='mt-56 ml-[15rem] rounded-full w-[20vw] h-[72vh] bg-green-400'
                />
                <div
-                  className='ml-12 mt-[30rem] rounded-full w-[16vw] h-[60vh] bg-green-400'
+                  style={{
+                     backgroundImage: "url('/assets/aboutDog.jpg')",
+                     backgroundPosition: "center",
+                     backgroundSize: "120%"
+                  }}
+                  className='ml-[5rem] mt-[28rem] rounded-full w-[16vw] h-[60vh] bg-green-400'
                />
             </section>
             <section
                className='z-30 flex flex-row w-full h-[110vh] relative bg-rice'
             >
                <div
-                  className='ml-[8rem] mt-[7rem] rounded-full w-[16vw] h-[55vh] bg-green-400'
+                  style={{
+                     backgroundImage: "url('/assets/aboutFood.jpg')",
+                     backgroundPosition: "left 60% bottom 40%",
+                     backgroundSize: "140%"
+                  }}
+                  className='ml-[8rem] mt-[9rem] rounded-full w-[16vw] h-[55vh] bg-green-400'
                />
                <div
-                  className='ml-12 mt-[2rem] rounded-full w-[20vw] h-[69vh] bg-green-400'
+                  style={{
+                     backgroundImage: "url('/assets/aboutCapybara.jpg')",
+                     backgroundPosition: "left 50% bottom 30%",
+                     backgroundSize: "150%"
+                  }}
+                  className='ml-16 rounded-full w-[20vw] h-[69vh] bg-green-400'
                />
                <div
-                  className='ml-32 mt-[18rem] rounded-full w-[20vw] h-[70vh] bg-green-400'
+                  style={{
+                     backgroundImage: "url('/assets/aboutOrchestra.jpg')",
+                     backgroundPosition: "left 50% bottom 30%",
+                     backgroundSize: "120%"
+                  }}
+                  className='ml-24 mt-[18rem] rounded-full w-[20vw] h-[70vh] bg-green-400'
                />
                <div
+                  style={{
+                     backgroundImage: "url('/assets/aboutSki.jpg')",
+                     backgroundPosition: "left 30% bottom 0%",
+                     backgroundSize: "95%"
+                  }}
                   className='ml-8 mt-[15rem] rounded-full w-[18vw] h-[45vh] bg-green-400'
                />
             </section>
